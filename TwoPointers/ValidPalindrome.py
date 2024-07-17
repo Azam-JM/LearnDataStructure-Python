cf3bc350617187a3db8470a1aebb9182a027b53f
Azam-JM <155773000+Azam-JM@users.noreply.github.com>
2024-07-17 22:28:41 +0100

    Create ValidPalindrome.py

---
+++ TwoPointers/ValidPalindrome.py
@@ -0,0 +1,10 @@
+# Write a function that takes a string, s, as an input and determines whether or not it is a palindrome.
+def is_palindrome(s):
+    left = 0
+  right = len(s) - 1
+  while left <= right:
+    if s[left] != s[right]:
+      return false
+    left  = left + 1
+    right = right - 1
+  return true
